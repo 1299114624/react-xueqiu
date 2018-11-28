b7cd2832e6e207ba877676710fe6cf16274951af
a45215 <791125264@qq.com>
2018-11-28 20:13:38 +0800

    ty

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -6,11 +6,15 @@ export default class Header extends Component{
     render(){
         return (
         <div id="home_header">
-            <ul>
-                <li>
+            <div className="banner__title">
+                <div className="banner_logo"></div>
+                <div className="banner__title__content">聪明的投资者都在这里</div>
+            </div>
+            <ul className="banner__btns">
+                <li className="banner_btns_left">
                     <a href="https://a.app.qq.com/o/simple.jsp?pkgname=com.xueqiu.android">在App内打开</a>
                 </li>
-                <li>
+                <li className="banner_btns_right">
                     <a href="https://a.app.qq.com/o/simple.jsp?pkgname=com.xueqiu.android">点击下载App</a>
                 </li>
             </ul>
